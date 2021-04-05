fef14050d4cfcc6a18c41c8f35d9836661812614
Seba <seba_msdstudio@hotmail.com>
2021-04-05 01:32:07 +0000

    Se agrego imagenes random

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 
 let urlapi  = 'https://danbooru.donmai.us/posts/'
 let prefix  = '~';
-fetch(urlapi)
+let num = Math.floor(Math.random() * ((4100000+1)-1)+1);
 
 client.on('ready', () => {
     console.log(`EL BOT  ${client.user.tag}! esta corriendo`);
@@ -15,17 +15,20 @@ client.on('message', msg => {
     let comand = msg.content.split(" ");
     let comand_v1 = comand[0];
     let comand_v2 = comand[1];
+
     if(comand_v1 == `${prefix}info`){
-        console.log();
+        console.log(msg);
     }
     if (comand_v1 === `${prefix}anime`) {
-        if(!isNaN(comand_v2)){
+        if(!isNaN(comand_v2) || comand_v2 == 'random'){
+            let id_num =  (comand_v2 == 'random')? Math.floor(Math.random() * ((4100000+1)-1)+1):comand_v2;
+            console.log(id_num);
             fetch(urlapi+comand_v2+'.json')
                 .then(promesaFetch => promesaFetch.json())
                 .then(contenido => {
                     if (contenido.rating == 'e' || contenido.rating== 'q') {
                         if (msg.channel.nsfw) {
-                            msg.channel.send('Is time to PAJA');
+                            msg.channel.send('Is time to PAJA 🍆🍌💦');
                             msg.channel.send(`${contenido.file_url}
                             obra: ${contenido.tag_string_copyright}
                             personajes: ${contenido.tag_string_character}
@@ -45,4 +48,4 @@ client.on('message', msg => {
         }
     }
 });
-client.login('the TOKEN');
+client.login('to key');
