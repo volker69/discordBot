7bce5ffb3824b7c625d574a1c1940a8bbdc338b8
Sebastian <seba_msdstudio@hotmail.com>
2021-04-11 07:33:46 +0000

    se agrego modulo, control de formato de imagenes e imagenes baneadas

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
 const fetch = require("node-fetch");
 const config = require('./config.json');
+const helps = require('./module')
+
+let num = helps.random_num;
+console.log(num);
 let urlapi  = 'https://danbooru.donmai.us/posts/';
-let prefix  = '~';
-let num = Math.floor(Math.random() * ((4100000+1)-1)+1);
+let prefix  = config.prefix;
+
 
 client.on('ready', () => {
     console.log(`EL BOT  ${client.user.tag}! esta corriendo`);
@@ -21,48 +25,70 @@ client.on('message', msg => {
         // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
         const embed = new MessageEmbed()
             .setTitle(`title`)
-            .setColor(0xff0000)
+            .setColor('#A7B960')
             .setDescription(`content`);
         msg.channel.send(embed);
       }
     if(comand_v1 == `${prefix}info`){
+        console.log("\`"+msg+"\`");
         console.log(msg);
     }
     if (comand_v1 === `${prefix}anime`) {
         if(!isNaN(comand_v2) || comand_v2 == 'random'){
-            let id_num =  (comand_v2 == 'random')? Math.floor(Math.random() * ((4500000+1)-1)+1):comand_v2;
-            console.log(id_num);
+            let id_num =  (comand_v2 == 'random')? num:comand_v2;
             fetch(urlapi+comand_v2+'.json')
                 .then(promesaFetch => promesaFetch.json())
                 .then(contenido => {
-                    if (contenido.rating == 'e' || contenido.rating== 'q') {
-                        if (msg.channel.nsfw) {
-                            const embed = new MessageEmbed()
-                                .setTitle(`Is time to PAJA 🍆🍌💦`)
-                                .setColor(0xff0000)
-                                .setDescription(`obra: ${contenido.tag_string_copyright}
-                                personajes: ${contenido.tag_string_character}
-                                artista: ${contenido.tag_string_artist}`)
-                                .setImage(contenido.file_url)
-                            msg.channel.send(embed);
+                    if (!contenido.is_banned) {
+                        if (contenido.file_ext == 'jpg' || contenido.file_ext == 'png' || contenido.file_ext == 'mp4') {
+                            if (contenido.rating == 'e' || contenido.rating== 'q') {
+                                if (msg.channel.nsfw) {
+                                    const embed = new MessageEmbed()
+                                        .setTitle(`Is time to PAJA 🍆🍌💦`)
+                                        .setColor(0xff0000)
+                                        .setDescription(`**obra:** ${contenido.tag_string_copyright}
+                                        **personajes:** ${contenido.tag_string_character}
+                                        **artista:** ${contenido.tag_string_artist}`)
+                                        .setImage(contenido.large_file_url)
+                                        .setFooter(`codigo ${contenido.id}`)
+                                    msg.channel.send(embed);
+                                }else{
+                                    const embed = new MessageEmbed()
+                                    .setTitle(`Donde esta tu Honor basura`)
+                                    .setColor(0xff0000)
+                                    .setImage(`https://pm1.narvii.com/6913/8d6065441de5f37af5c8d835995fe63365ff1d18r1-480-299v2_hq.jpg`)
+                                    .setFooter(`codigo nuclear ${contenido.id}`)
+                                msg.channel.send(embed);
+                                }
+                            }else{
+                                const embed = new MessageEmbed()
+                                    .setTitle(`Una Imagen Relax`)
+                                    .setColor('#A7B960')
+                                    .setDescription(`**obra:** ${contenido.tag_string_copyright}
+                                    **personajes:** ${contenido.tag_string_character}
+                                    **artista:** ${contenido.tag_string_artist}`)
+                                    .setImage(contenido.large_file_url)
+                                    .setFooter(`codigo  ${contenido.id}`)
+                                msg.channel.send(embed)
+                            }
                         }else{
                             const embed = new MessageEmbed()
-                            .setTitle(`Donde esta tu Honor basura`)
-                            .setColor(0xff0000)
-                            .setImage(`https://pm1.narvii.com/6913/8d6065441de5f37af5c8d835995fe63365ff1d18r1-480-299v2_hq.jpg`)
-                            .setFooter(`codigo nuclear ${contenido.id}`)
-                        msg.channel.send(embed);
+                                    .setTitle(`IMAGEN NO DISPONIBLE`)
+                                    .setColor('#A7B960')
+                                    .setImage('https://memegenerator.net/img/instances/65961959/pero-mira-como-esta-ese-contenido-no-disponible-pap.jpg')
+                                msg.channel.send(embed)
                         }
                     }else{
                         const embed = new MessageEmbed()
-                            .setTitle(`Una Imagen Relax`)
-                            .setColor(0xff0000)
-                            .setDescription(`obra: ${contenido.tag_string_copyright}
-                            personajes: ${contenido.tag_string_character}
-                            artista: ${contenido.tag_string_artist}`)
-                            .setImage(contenido.file_url)
-                        msg.channel.send(embed)
+                        .setTitle(`IMAGEN BENEADA`)
+                        .setColor('#3da6bd')
+                        .setDescription(`**obra:** ${contenido.tag_string_copyright}
+                        **personajes:** ${contenido.tag_string_character}
+                        **artista:** ${contenido.tag_string_artist}`)
+                        .setImage('https://cdn.memegenerator.es/imagenes/memes/full/30/98/30982970.jpg')
+                    msg.channel.send(embed)
                     }
+                    
                     })
         }else{
             msg.channel.send('Upsi! , comando invalido')
---
+++ module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fetch = require("node-fetch");
+
+module.exports = {
+    random_num: Math.floor(Math.random() * ((4500000+1)-1)+1)
+}
+
+
